439cb28e0395144e3d4085ffd1c4cecb0933a9db
PeterSolution <piotrwolff@op.pl>
2024-06-10 23:18:14 +0200

    first

---
+++ Controllers/DataController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : Controller
+    {
+        // GET: data
+        [HttpGet]
+        public Dataa get()
+        {
+            var dataa = new Dataa
+            {
+                Id = 1,
+                Name = "Test"
+            };
+            return dataa;
+        }
+        [HttpPost]
+        public void post(int id, string name)
+        {
+
+        }
+        [HttpPut("{put}")]
+        public void put(int id,string name)
+        {
+
+        }
+        [HttpDelete]
+        public void delete(int id)
+        {
+
+        }
+    }
+}
---
+++ Dataa.cs
@@ -0,0 +1,8 @@
+﻿namespace backend
+{
+    public class Dataa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
